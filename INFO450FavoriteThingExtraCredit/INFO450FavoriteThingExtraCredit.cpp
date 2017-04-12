80b3d732d30af40450752c922a85bb4b1ee5ce38
James Nicholson <nicholsonjb@vcu.edu>
2017-04-12 07:17:57 -0400

    Delete Method Extra Credit

--- INFO450FavoriteThingExtraCredit/INFO450FavoriteThingExtraCredit.cpp
+++ INFO450FavoriteThingExtraCredit/INFO450FavoriteThingExtraCredit.cpp
@@ -1,3 +1,6 @@
+// INFO450FavoriteThingExtraCredit.cpp : Defines the entry point for the console application.
+//
+
 // INFO450FavoriteThing.cpp : Defines the entry point for the console application.
 //
 
@@ -54,6 +57,8 @@ public:
 	int saveBeerList(string filename);
 	int readBeerList(string filename);
 	void addNewBeer();
+	void deleteingBeer(string filename, Beer* name);
+	void searchingBeer();
 };
 
 
@@ -271,6 +276,26 @@ int BeerList::readBeerList(string filename)
 	return 0;
 }
 
+//Deleting Favorite Beer
+void BeerList::deleteingBeer(string filename, Beer* name)
+{
+	string line,ans;
+	cout << "Please enter the name of the beer you want to delete: " << endl;
+	getline(cin, ans);
+	ifstream myfile(filename);
+	ofstream temp;
+	myfile.open(filename);
+	temp.open("temp.txt");
+	while (getline(myfile, line))
+	{
+		if (line != name)
+			temp << line << endl;
+	}
+	cout << "The record with the name " << name << "has been deleted if it existed" << endl;
+	myfile.close();
+	temp.close();
+}
+
 
 int main()
 {
@@ -300,5 +325,6 @@ int main()
 	my.getUserInput();
 	my.saveBeerList(filename);
 	my.showBeerList();
+	my.deleteingBeer(filename);
 	return 0;
 }
